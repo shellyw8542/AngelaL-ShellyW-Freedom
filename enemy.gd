115f66e4e8aafda5e9bc0bc9d0a50b03233814a6
shellyw8542 <114585796+shellyw8542@users.noreply.github.com>
2025-04-19 12:48:25 -0400

    enemy finished

--- enemy.gd
+++ enemy.gd
@@ -1,16 +1,23 @@
 extends CharacterBody3D
 
-@onready var nav_agent =$"/root/World/Enemy/NavigationRegion3D"
+@onready var nav_agent =$NavigationAgent3D
 
 var SPEED = 3.0 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var current_location = global_transform.origin
-	var next_location = nav_agent.get_next_path_position()
+	var next_location =  nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * SPEED 
 	
-	velocity = new_velocity
-	move_and_slide()
+	nav_agent.set_velocity(new_velocity)
 
 func update_target_location(target_location):
 	nav_agent.target_position = target_location
+
+func _on_navigation_agent_3d_target_reached():
+	pass
+
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity):
+	velocity = velocity.move_toward(safe_velocity, 0.25)
+	move_and_slide()
--- node_3d.gd
+++ node_3d.gd
@@ -2,5 +2,5 @@ extends Node3D
 
 @onready var player = $ProtoController
 
-func _physic_process(delta):
+func _physics_process(delta):
 	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
